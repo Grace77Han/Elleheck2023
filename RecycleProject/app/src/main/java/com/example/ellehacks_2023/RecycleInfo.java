5339d3474faabeab21875b3754a2cd7647432718
Grace77Han <grace77han@gmail.com>
2023-02-18 20:09:58 -0500

    Update RecycleInfo.java

--- RecycleProject/app/src/main/java/com/example/ellehacks_2023/RecycleInfo.java
+++ RecycleProject/app/src/main/java/com/example/ellehacks_2023/RecycleInfo.java
@@ -4,114 +4,113 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class RecycleInfo {
-    List<String> recyclable;
-    List<String> nonRecyclable;
+    List<String> blueBin;
+    List<String> garbage;
 
     public RecycleInfo() {
-        recyclable = new ArrayList<>();
-        nonRecyclable = new ArrayList<>();
+        blueBin = new ArrayList<>();
+        garblueBin ArrayList<>();
 
         loadRecycleInfo();
-        loadNonRecycleInfo();
+        loadGarbageInfo();
     }
 
     private void loadRecycleInfo() { // Helper for constructor: It updates arrayLists
-        recyclable.add("glass");
-        recyclable.add("glass bottle");
-        recyclable.add("glass jar");
-        recyclable.add("rigid plastic");
-        recyclable.add("plastic jar");
-        recyclable.add("plastic tub");
-        recyclable.add("plastic food container");
-        recyclable.add("beverage bottle");
-        recyclable.add("salad dressing bottle");
-        recyclable.add("detergent bottle");
-        recyclable.add("soap bottle");
-        recyclable.add("shampoo bottle");
-        recyclable.add("cd case");
-        recyclable.add("dvd case");
-        recyclable.add("paper bag");
-        recyclable.add("avertising mail");
-        recyclable.add("fine paper");
-        recyclable.add("envelope");
-        recyclable.add("gift wrap");
-        recyclable.add("tissue paper");
-        recyclable.add("card");
-        recyclable.add("shredded paper");
-        recyclable.add("soft cover book");
-        recyclable.add("hard cover book");
-        recyclable.add("paper container");
-        recyclable.add("carton");
-        recyclable.add("box");
-        recyclable.add("sprial wound can");
-        recyclable.add("cardboard");
-        recyclable.add("boxboard");
-        recyclable.add("detergent box");
-        recyclable.add("shoe box");
-        recyclable.add("tissue box");
-        recyclable.add("roll");
-        recyclable.add("toilet paper box");
-        recyclable.add("paper towel box");
-        recyclable.add("wapping paper");
-        recyclable.add("foam p]olystyrene");
-        recyclable.add("foam food package");
-        recyclable.add("foam plate");
-        recyclable.add("foam cup");
-        recyclable.add("egg carton");
-        recyclable.add("takeout container");
-        recyclable.add("meat tray");
-        recyclable.add("fish tray");
-        recyclable.add("protective packaging");
-
+        blueBin.add("glass");
+        blueBin.add("glass bottle");
+        blueBin.add("glass jar");
+        blueBin.add("rigid plastic");
+        blueBin.add("plastic jar");
+        blueBin.add("plastic tub");
+        blueBin.add("plastic food container");
+        blueBin.add("beverage bottle");
+        blueBin.add("salad dressing bottle");
+        blueBin.add("detergent bottle");
+        blueBin.add("soap bottle");
+        blueBin.add("shampoo bottle");
+        blueBin.add("cd case");
+        blueBin.add("dvd case");
+        blueBin.add("paper bag");
+        blueBin.add("avertising mail");
+        blueBin.add("fine paper");
+        blueBin.add("envelope");
+        blueBin.add("gift wrap");
+        blueBin.add("tissue paper");
+        blueBin.add("card");
+        blueBin.add("shredded paper");
+        blueBin.add("soft cover book");
+        blueBin.add("hard cover book");
+        blueBin.add("paper container");
+        blueBin.add("carton");
+        blueBin.add("box");
+        blueBin.add("sprial wound can");
+        blueBin.add("cardboard");
+        blueBin.add("boxboard");
+        blueBin.add("detergent box");
+        blueBin.add("shoe box");
+        blueBin.add("tissue box");
+        blueBin.add("roll");
+        blueBin.add("toilet paper box");
+        blueBin.add("paper towel box");
+        blueBin.add("wapping paper");
+        blueBin.add("foam p]olystyrene");
+        blueBin.add("foam food package");
+        blueBin.add("foam plate");
+        blueBin.add("foam cup");
+        blueBin.add("egg carton");
+        blueBin.add("takeout container");
+        blueBin.add("meat tray");
+        blueBin.add("fish tray");
+        blueBin.add("protective packaging");
     }
 
-    private void loadNonRecycleInfo() { // Helper for constructor: It updates arrayLists
-        nonRecyclable.add("milk container");
-        nonRecyclable.add("juice bottle");
-        nonRecyclable.add("foam packaging peanut");
-        nonRecyclable.add("medical waste");
-        nonRecyclable.add("healthcare waste");
-        nonRecyclable.add("protective equipment");
-        nonRecyclable.add("needle");
-        nonRecyclable.add("syringe");
-        nonRecyclable.add("disposable mask");
-        nonRecyclable.add("gloves");
-        nonRecyclable.add("gown");
-        nonRecyclable.add("pad");
-        nonRecyclable.add("soiled dressing");
-        nonRecyclable.add("sponge");
-        nonRecyclable.add("gauze");
-        nonRecyclable.add("catheter");
-        nonRecyclable.add("colostomy bag");
-        nonRecyclable.add("IV bag");
-        nonRecyclable.add("IV tubing");
-        nonRecyclable.add("dialysis waste");
-        nonRecyclable.add("household hazardous waste");
-        nonRecyclable.add("propane");
-        nonRecyclable.add("helium tank");
-        nonRecyclable.add("battery");
-        nonRecyclable.add("compact fluorescent light bulb");
-        nonRecyclable.add("pesticide");
-        nonRecyclable.add("paint");
-        nonRecyclable.add("plastic packaging");
-        nonRecyclable.add("black plastic");
-        nonRecyclable.add("black plastic bag");
-        nonRecyclable.add("black plastic container");
-        nonRecyclable.add("black plant tray");
-        nonRecyclable.add("black flower pot");
-        nonRecyclable.add("compostable bag");
-        nonRecyclable.add("compostable container");
-        nonRecyclable.add("compostable cutlery");
+    private void loadGarbageInfo() { // Helper for constructor: It updates arrayLists
+        garbage.add("milk container");
+        garbage.add("juice bottle");
+        garbage.add("foam packaging peanut");
+        garbage.add("medical waste");
+        garbage.add("healthcare waste");
+        garbage.add("pgarbageipment");
+        garbage.add("needle");
+        garbage.add("syringe");
+        garbage.add("disposable mask");
+        garbage.add("gloves");
+        garbage.add("gown");
+        garbage.add("pad");
+        garbage.add("soiled dressing");
+        garbage.add("sponge");
+        garbage.add("gauze");
+        garbage.add("catheter");
+        garbage.add("colostomy bag");
+        garbage.add("IV bag");
+        garbage.add("IV tubing");
+        garbage.add("dialysis waste");
+        garbage.add("household hazardous waste");
+        garbage.add("propane");
+        garbage.add("helium tank");
+        garbage.add("battery");
+        garbage.add("compact fluorescent light bulb");
+        garbage.add("pesticide");
+        garbage.add("paint");
+        garbage.add("plastic packaging");
+        garbage.add("black plastic");
+        garbage.add("black plastic bag");
+        garbage.add("black plastic container");
+        garbage.add("black plant tray");
+        garbage.add("black flower pot");
+        garbage.add("compostable bag");
+        garbage.add("compostable container");
+        garbage.add("compostable cutlery");
         // (Yeseul) I need to add more info!
     }
 
-    public String isRecyclable(String item) { // Helper for search() : it checks if the item is in any list
+    public String getType(String item) { // Helper for search() : it checks if the item is in any list
         String itemLowercase = item.toLowerCase(); // for removing case sensitive
 
-        if (recyclable.contains(itemLowercase)) {
-            return "recycle";
+        if (blueBin.contains(itemLowercase)) {
+            return "blue bin";
         }
-        else if (nonRecyclable.contains(itemLowercase)) {
+        else if (garbage.contains(itemLowercase)) {
             return "garbage";
         }
         else { // Can't search that such object
@@ -124,10 +123,10 @@ public class RecycleInfo {
      */
 //    public void search(String s) {
 //
-//        if (isRecyclable(s) == "Yes") {
+//        if (getType == "blue bin") {
 //            // Need a function to show blue-bin result (maybe pop up?)
 //        }
-//        else if (isRecyclable(s) == "No") {
+//        else if (getType == "garbage") {
 //            // Need a function to show grey-bin result (maybe pop up?)
 //        }
 //        else {
